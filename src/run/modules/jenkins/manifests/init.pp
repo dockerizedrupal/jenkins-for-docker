d86632e200903912f00605aa975c82713057245b
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-07 05:54:49 +0200

    Update

--- src/run/modules/jenkins/manifests/init.pp
+++ src/run/modules/jenkins/manifests/init.pp
@@ -1,13 +1,5 @@
 class jenkins {
-  file { '/jenkins/data/config.xml':
-    ensure => present,
-    source => 'puppet:///modules/jenkins/jenkins/data/config.xml',
-    mode => 644,
-    replace => no
-  }
-
   exec { 'chown -R jenkins.jenkins /jenkins':
-    path => ['/bin'],
-    require => File['/jenkins/data/config.xml']
+    path => ['/bin']
   }
 }
