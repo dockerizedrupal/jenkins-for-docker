f3debbd220327e989b245133c03a73f6a4ec009b
Jürgen Viljaste <j.viljaste@gmail.com>
2015-01-29 21:37:48 +0200

    Update

--- src/build/modules/jenkins/manifests/machine.pp
+++ src/build/modules/jenkins/manifests/machine.pp
@@ -1,7 +1,8 @@
 class jenkins::machine {
   file { '/tmp/docker-machine_linux-amd64':
     ensure => present,
-    source => 'puppet:///modules/jenkins/tmp/docker-machine_linux-amd64'
+    source => 'puppet:///modules/jenkins/tmp/docker-machine_linux-amd64',
+    mode => 755
   }
 
   exec { 'mv /tmp/docker-machine_linux-amd64 /usr/local/bin/machine':
