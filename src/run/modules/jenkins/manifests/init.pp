6b0fd0170d5fec5552c17c46197cb3ce39f30112
Jürgen Viljaste <j.viljaste@gmail.com>
2015-01-31 09:46:40 +0200

    Update

--- src/run/modules/jenkins/manifests/init.pp
+++ src/run/modules/jenkins/manifests/init.pp
@@ -1,8 +1,13 @@
 class jenkins {
   require jenkins::nginx
 
-  exec { 'mkdir -p /jenkins/data':
+  exec { 'mkdir -p /jenkins/home':
     path => ['/bin']
+  }
+
+  exec { 'mkdir -p /jenkins/data':
+    path => ['/bin'],
+    require => Exec['mkdir -p /jenkins/home']
   }
 
   exec { 'chown -R jenkins.jenkins /jenkins':
