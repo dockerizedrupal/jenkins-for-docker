0644b4d2d10b942685ad7932616a3a10745fd168
Jürgen Viljaste <j.viljaste@gmail.com>
2015-06-27 23:02:44 +0300

    Update

--- tools/jenkinsdata.sh
+++ tools/jenkinsdata.sh
@@ -14,7 +14,7 @@ fi
 
 help() {
   cat << EOF
-jenkinsdata: Usage: jenkinsdata <backup|restore|rm>
+jenkinsdata: Usage: jenkinsdata <backup|restore>
 EOF
 
   exit 1
@@ -70,12 +70,6 @@ elif [ "${1}" = "restore" ]; then
       --entrypoint /bin/bash \
       viljaste/base:latest -c "tar xzvf /backup/${CONTAINER}.tar.gz"
   done
-elif [ "${1}" = "rm" ]; then
-  CONTAINERS="$(jenkinsdata_containers)"
-
-  if [ -n "${CONTAINERS}" ]; then
-    docker rm -f ${CONTAINERS}
-  fi
 else
   unknown_command
 fi
